70bd4872d712e777381b1947a312ea8ea561ec66
Luiz <luizhmatoso1340@gmail.com>
2024-07-08 14:17:38 -0300

    Project created

---
+++ src/test/java/com/educandoweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
